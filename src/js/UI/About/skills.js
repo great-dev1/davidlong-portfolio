6a8f81d589f81f5567f564eeaf2dc4e62bd67463
David <twentytwentyaugust@outlook.com>
2023-11-14 18:40:21 +0900

    init

---
+++ src/js/UI/About/skills.js
@@ -0,0 +1,35 @@
+export default [
+    
+    {
+        name: 'React/Next.js',
+        width: '100%',
+    },
+    {
+        name: 'Vue/Nuxt.js',
+        width: '100%'
+    },
+    {
+        name: 'Laravel',
+        width: '100%',
+    },
+    {
+        name: 'Python/Django',
+        width: '90%',
+    },
+    {
+        name: 'nodeJS/ExpressJS',
+        width: '100%',
+    },
+    {
+        name: 'Wordpress/Shopify',
+        width: '90%',
+    },
+    {
+        name: 'Webflow',
+        width: '100%',
+    },
+    {
+        name: 'Responsive Design',
+        width: '100%',
+    },
+]
